98f07b6334851ecb3d39c4d94509f68b74a6e707
Ana Lopes <anapedra.mil@gmail.com>
2022-11-23 22:41:59 -0300

    Database seeding

--- backend/src/main/java/com/anasatanaslopessantantospedra/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/anasatanaslopessantantospedra/dscatalog/DscatalogApplication.java
@@ -1,11 +1,23 @@
 package com.anasatanaslopessantantospedra.dscatalog;
 
+import com.anasatanaslopessantantospedra.dscatalog.model.Category;
+import com.anasatanaslopessantantospedra.dscatalog.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class DscatalogApplication implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    public DscatalogApplication(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DscatalogApplication.class, args);
@@ -13,6 +25,12 @@ public class DscatalogApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category category1=new Category(null,"Books");
+        Category category2=new Category(null,"Electronics");
+        Category category3=new Category(null,"Computers");
+
+        categoryRepository.saveAll(Arrays.asList(category1,category2,category3));
 
 
     }
--- backend/src/main/java/com/anasatanaslopessantantospedra/dscatalog/service/CategoryService.java
+++ backend/src/main/java/com/anasatanaslopessantantospedra/dscatalog/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.anasatanaslopessantantospedra.dscatalog.repository.CategoryRepository
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryService {
@@ -13,6 +14,7 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+    @Transactional(readOnly = true)
     public Page<Category> findAllCategory(Pageable pageable){
         Page<Category> categories=categoryRepository.findAll(pageable);
         return categories;
